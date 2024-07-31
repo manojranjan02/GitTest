e073d8fe9f75b796a5e74cf2d01ebca3d57ab1bc
Manoj <manoj1260555@gmail.com>
2024-07-31 13:32:34 +0530

    Revent

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GitTest.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> UserAccount()
+        {
+            return View();
+        }
+    }
+}
